cfb1addb690dfb227114050c3b9bbdc367f4a5f9
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-06-23 01:30:58 +0300

    minor changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,10 @@
 //import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+//import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    prerender: {
-      default: true
-    }
-  },
-
 	preprocess: [
     preprocess({
       postcss: true
@@ -17,7 +12,10 @@ const config = {
   ],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+    prerender: {
+      default: true
+    }
 	}
 };
 
