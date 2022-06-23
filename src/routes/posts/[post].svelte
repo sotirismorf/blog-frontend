e7fc9721f66638f25e418c97481fe7bd1bd12126
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-06-23 05:10:46 +0300

    individual post screen fix

--- src/routes/posts/[post].svelte
+++ src/routes/posts/[post].svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Card from "$lib/Card.svelte";
-	export let post: any
+	import categories from '$lib/categories'
+	export let post: App.Post
 
 	import Article from "$lib/Article.svelte";
 </script>
@@ -13,7 +14,12 @@
 	<Card>
 		<div class="bg-light-darker">
 			<h1 class="text-4xl font-bold mb-3">{post.title}</h1>
-			<img class="rounded-t-lg"  src="/img/{post.fmedia}" />
+			<p class="text-xl mb-3 italic text-green">{categories[post.categories]}</p>
+			<img
+				alt={post.fmedia[0]}
+				class="rounded-lg m-auto my-6"
+				src="https://picsum.photos/id/{post.fmedia}/200/250" 
+			/>
 			<p class="text-lg">{post.body}</p>
 		</div>
 	</Card>
