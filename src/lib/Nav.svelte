774e449cd7a7bfea9ba1b25d9ee115decc331a03
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-06-23 05:09:43 +0300

    Nav bar change

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -11,11 +11,11 @@
 	]
 </script>
 
-<nav class="text-center py-3 bg-light-darker shadow-md">
-	<a class="text-2xl pr-5" href="/"><b>gizmo_lab</b></a>
+<nav class="text-center py-3 bg-light-darker shadow-md select-none">
+	<a class="text-2xl pr-5 italic font-['Helvetica']" href="/"><b>gizmo_lab</b></a>
 	{#each items as item}
 		<a
-			class="p-3 text-xl"
+			class="p-3 text-xl hover:text-orange transition-all"
 			href={item.href}
 		>{item.description}</a>
 	{/each}
