37e4d3459a01e545cb51740bd2eccfd0653ad1a0
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-06-23 05:11:10 +0300

    minor config change

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      'mono': ['Roboto\\ Mono', ...defaultTheme.fontFamily.mono],
+    },
     colors: {
     'light': '#FAF3E3',
     'light-darker': '#F0E9DA',
